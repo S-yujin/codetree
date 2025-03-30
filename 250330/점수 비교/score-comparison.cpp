59ff55a5a4a608b160e33b1e918ac8aa9442c735
songyujin <syuj2412@gmail.com>
2025-03-30 09:12:35 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250330/점수 비교/score-comparison.cpp
+++ 250330/점수 비교/score-comparison.cpp
@@ -5,8 +5,8 @@ int main() {
     int math_A, math_B;
     int eng_A, eng_B;
 
-    cin >> math_A >> math_B;
-    cin >> eng_A >> eng_B;
+    cin >> math_A >> eng_A;
+    cin >> math_B >> eng_B;
 
     if(math_A > math_B && eng_A > eng_B)
         cout << "1" << endl;
