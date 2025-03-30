83306ddf106ed2137dcda5f4e8c743e5093b6750
songyujin <syuj2412@gmail.com>
2025-03-30 09:00:03 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250330/점수 비교/score-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int math_A, math_B;
+    int eng_A, eng_B;
+
+    cin >> math_A >> math_B;
+    cin >> eng_A >> eng_B;
+
+    if(math_A > math_B && eng_A > eng_B)
+        cout << "1" << endl;
+    else
+        cout << "0" <<endl;
+    return 0;
+}
