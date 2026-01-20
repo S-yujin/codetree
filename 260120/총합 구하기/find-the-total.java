821c1b975f0462dfd47d002af8a7e9a6248fdfca
송유진 <syuj2412@gmail.com>
2026-01-20 21:57:22 +0900

    Title: 총합 구하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260120/총합 구하기/find-the-total.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int sum = 0;
+
+        for(int i = a; i <= b; i++){
+            if(i % 6 == 0 && i % 8 == 1){
+                sum += i;
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
