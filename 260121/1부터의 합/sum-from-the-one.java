22f90a847c631ea8a5a15089d3beeb142eac04d4
송유진 <syuj2412@gmail.com>
2026-01-21 16:24:11 +0900

    Title: 1부터의 합, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 260121/1부터의 합/sum-from-the-one.java
+++ 260121/1부터의 합/sum-from-the-one.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
         int n = in.nextInt();
+
         int sum = 0;
-        int cnt = 0;
-        for(int i = 1; i <= 100; i++){
-            if(sum >= n){
-                System.out.print(sum);
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            if (sum >= n) {
+                System.out.print(i);
                 break;
             }
         }
