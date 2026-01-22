b499a062ff33e94005b035a30deeb65480e488fc
송유진 <syuj2412@gmail.com>
2026-01-22 16:28:41 +0900

    Title: 몫 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260122/몫 출력하기/print-share.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int cnt = 0;
+
+        while(true){
+            int n = in.nextInt();
+
+            if(n % 2 == 0){
+                System.out.println(n/2);
+                cnt ++;
+            }
+
+            if(cnt == 3){
+                break;
+            }
+        }
+    }
+}
