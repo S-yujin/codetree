b078ab38e011c71335dff0120bc25326fcbc4563
송유진 <syuj2412@gmail.com>
2026-01-24 21:45:55 +0900

    Title: 별표 출력하기 4, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260124/별표 출력하기 4/print-star-4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for (int i = 1; i <= n; i++) { 
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 2; i >= 0; i--) {    
+            for (int j = 1; j <= n - i; j++) {   
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
