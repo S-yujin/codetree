aedb9411b219e1349449b5938d26b5b4850eb04d
송유진 <syuj2412@gmail.com>
2026-01-27 21:58:26 +0900

    Title: 1-9 정사각형, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260127/1-9 정사각형/1-9-square.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+
+        int cnt = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(cnt);
+                cnt++;
+
+                if (cnt == 10) {
+                    cnt = 1;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
