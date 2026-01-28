c41cbf0da7149f104341530dcc8db82493b74be0
송유진 <syuj2412@gmail.com>
2026-01-28 22:12:12 +0900

    Title: 구구단 만들기, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260128/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        for(int i = 1; i < 10; i++){
+            for(int j = b; j > a - 1; j -= 2){
+                System.out.print(j + " * " + i + " = " + i*j);
+                if(j > a){
+                    System.out.print(" / ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
