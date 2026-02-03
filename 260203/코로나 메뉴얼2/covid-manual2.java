13ce097476e4b3d0d2720a95f8876f841b14954c
송유진 <syuj2412@gmail.com>
2026-02-03 12:11:55 +0900

    Title: 코로나 메뉴얼2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260203/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A = 0, B = 0, C = 0, D = 0;
+        boolean emergency = false;
+
+        for (int i = 0; i < 3; i++) {
+            String cold = in.next();
+            int temp = in.nextInt();
+
+            if (temp >= 40) {
+                emergency = true;
+            }
+
+            if (cold.equals("Y")) {
+                if (temp >= 37) A++;
+                else C++;
+            } else {
+                if (temp >= 37) B++;
+                else D++;
+            }
+        }
+
+        System.out.print(A + " " + B + " " + C + " " + D);
+        if (emergency) {
+            System.out.print(" E");
+        }
+    }
+}
