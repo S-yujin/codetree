66a3f41a500692d83db403c0d9a70407e4c36926
송유진 <syuj2412@gmail.com>
2026-02-04 21:12:18 +0900

    Title: 연속부분수열일까, Time: 142ms, Memory: 10MB, Status: Passed - Codetree

--- 260204/연속부분수열일까/contiguous-array-or-not.java
+++ 260204/연속부분수열일까/contiguous-array-or-not.java
@@ -4,44 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n1, n2, i, j, cnt;
-
-        cnt = 0;
-        
-        n1 = sc.nextInt();
-        n2 = sc.nextInt();
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
 
         int[] a = new int[n1];
         int[] b = new int[n2];
 
-        for(i = 0 ; i < n1 ; i ++){
+        for (int i = 0; i < n1; i++) {
             a[i] = sc.nextInt();
         }
-        for(i = 0 ; i < n2 ; i ++){
+        for (int i = 0; i < n2; i++) {
             b[i] = sc.nextInt();
         }
 
-        for(i = 0 ; i < n1 ; i ++){
-            if(a[i] == b[0]){
-                for(j = 0 ; j < n2 ; j ++){
-                    if(a[i + j] == b[j]){
-                        cnt ++;
-                    }
-                }
-                if(cnt == n2){
-                    System.out.print("Yes");
+        boolean found = false;
+
+        for (int i = 0; i <= n1 - n2; i++) {
+            int cnt = 0;
+
+            for (int j = 0; j < n2; j++) {
+                if (a[i + j] == b[j]) {
+                    cnt++;
+                } else {
                     break;
                 }
+            }
 
-                else{
-                    cnt = 0;
-                }
+            if (cnt == n2) {
+                found = true;
+                break;
             }
         }
-        
-        if(cnt == 0){
+
+        if (found) {
+            System.out.print("Yes");
+        } else {
             System.out.print("No");
         }
-
     }
 }
