56f287ccb69b4971c607f5732637e9145c67457f
송유진 <syuj2412@gmail.com>
2026-02-12 21:12:13 +0900

    Title: 문자열의 길이 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260212/문자열의 길이 출력하기/print-strings-length.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String str = in.next();
+        String str2 = in.next();
+
+        System.out.print(str.length() + str2.length);
+    }
+}
