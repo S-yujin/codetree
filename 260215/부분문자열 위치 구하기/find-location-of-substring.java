c47e2f851588786bad88a8bc00654a833893d736
송유진 <syuj2412@gmail.com>
2026-02-15 23:35:58 +0900

    Title: 부분문자열 위치 구하기, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260215/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String str = in.next();
+
+        String sss = in.next();
+
+
+        int n = str.indexOf(sss);
+
+        System.out.print(n);
+
+
+    }
+}
