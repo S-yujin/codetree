3ba4a59c6ba7259559ba5c95811a83192bd6a312
송유진 <syuj2412@gmail.com>
2026-02-21 21:25:35 +0900

    Title: 붙여서 합하기, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260221/붙여서 합하기/add-and-add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        String B = sc.next();
+
+        String AB = A + B;
+        String BA = B + A;
+
+        int result = Integer.parseInt(AB) + Integer.parseInt(BA);
+
+        System.out.println(result);
+    }
+}
