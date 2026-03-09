b506e762bd7261b7fda5b19dea6f4f201f985f9a
송유진 <syuj2412@gmail.com>
2026-03-09 16:04:37 +0900

    Add files via upload

---
+++ 260202/일의 자리 배열/array-with-units.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        arr[0] = in.nextInt();
+        arr[1] = in.nextInt();
+
+        System.out.print(arr[0] + " ");
+        System.out.print(arr[1] + " ");
+
+        for (int i = 2; i < 10; i++) {
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 10;
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
